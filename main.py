0437f19d38ebc78b76fb192614280e89615b63cd
Negolas Cage <franko.macetic@net.hr>
2017-01-16 22:02:09 +0100

    Update main.py
    minimizers now also contain information about the direction they were retrieved from (original sequence or its reverse complement)
    read number index for minimizers got from the reverse complement of the sequence now match the read number index of the reverse complement of the sequence if sequence were to be written in the same way (using same read sizes) as the input file.

--- main.py
+++ main.py
@@ -15,32 +15,30 @@ def print_minimizers(count):
 
 class Minimizer():
     """A class containing the string of a minimizer and all locations it
-        was found at (as well as the direction)."""
+        was found at as well as the direction of the read."""
 
     def __init__(self, string, loc):
-        """String should be k characters long, loc should contain
-              only one pair of coordinates i.e. [i, p]."""
+        """String should be k characters long, loc should contain only
+              one pair of coordinates i.e. [i, p]."""
         self.string = string
         self.locations = [loc]
 
     def __lt__(self, other):
         return self.string < other.string
 
-    def __eq__(self, other):
-        return self.string == other.string
-
     def __repr__(self):
         return '({0}, {1})'.format(self.string, self.locations)
 
     def add_location(self, loc):
         """ Add a pair of coordinates to the minimizer. loc should
-              contain two elements [i, p], where p is read number and i
-              is offset in that read."""
+              contain three elements [i, p, d], where p is read number,
+              i is the offset in that read, and d is True if minimizer
+              is from the RC of the sequence."""
         if loc not in self.locations:
             self.locations.append(loc)
 
     def get_locations(self):
-        """Returns an array of all location pairs."""
+        """Returns an array of all location triples."""
         return self.locations
 
     def get_number(self):
@@ -54,7 +52,7 @@ class Minimizer():
 
 def get_rc(sequence):
     """Returns the reverse complement of sequence. Sequence may only
-        contain letters A, C, G, or T."""
+        contain letters A, C, G, or T (in uppercase)."""
     table = ['A', 'C', 'G', 'T']
 
     sequence = list(sequence)
@@ -81,13 +79,19 @@ def get_minimizer_string_index(string):
 
 def get_minimizer_from_window(begin_index, end_index, i, w, k):
     """Returns all minimizers for the window[begin_index:end_index]."""
+
     current_window = CURRENT_READ[begin_index:end_index]
 
     minimizer = None
     for p in range(w):
         # making sure we didn't go past the last possible k-mer
         if len(current_window[p:]) >= k:
-            temp = Minimizer(''.join(current_window[p:p + k]), [i, p + begin_index])
+            if not USING_RC:
+                temp = Minimizer(''.join(current_window[p:p + k]), \
+                                 [i, p + begin_index, USING_RC])
+            else:
+                temp = Minimizer(''.join(current_window[p:p + k]), \
+                                 [1670-1-i, p + begin_index, USING_RC])
         else:
             break
 
@@ -107,23 +111,32 @@ def get_minimizer_lookalike(minimizer):
 
     return MINIMIZERS[index] if index != -1 else None
 
-def get_end_minimizers(k):
-    """Saves left and right k-end-minimizers for the current read."""
-    left_minimizer = Minimizer(CURRENT_READ[:k], [READ_NUMBER, 0])
-    right_minimizer = Minimizer(CURRENT_READ[-k:], [READ_NUMBER, len(CURRENT_READ) - k])
+def calculate_end_minimizers(k):
+    """Calculates left and right k-end-minimizers for the current read
+    and inserts them into MINIMIZERS list."""
+    if not USING_RC:
+        left_minimizer = Minimizer(CURRENT_READ[:k], [READ_NUMBER, 0, USING_RC])
+        right_minimizer = Minimizer(CURRENT_READ[-k:], \
+                                    [READ_NUMBER, len(CURRENT_READ) - k, USING_RC])
+    else:
+        left_minimizer = Minimizer(CURRENT_READ[:k], \
+                                   [TOTAL_READS - READ_NUMBER - 1, 0, USING_RC])
+        right_minimizer = Minimizer(CURRENT_READ[-k:], \
+                                    [TOTAL_READS - READ_NUMBER - 1, \
+                                     len(CURRENT_READ) - k, USING_RC])
 
     if not get_minimizer_lookalike(left_minimizer):
         insort(MINIMIZERS, left_minimizer)
-        #MINIMIZERS.append(left_minimizer)
+##        MINIMIZERS.append(left_minimizer)
 ##        temp[left_minimizer.string] = left_minimizer
     if not get_minimizer_lookalike(right_minimizer):
         insort(MINIMIZERS, right_minimizer)
-        #MINIMIZERS.append(right_minimizer)
+##        MINIMIZERS.append(right_minimizer)
 ##        temp[right_minimizer.string] = right_minimizer
 
-#@profile
-def get_interior_minimizers(w, k):
-    """Saves all interior minimizers for the window size = w + k - 1."""
+def calculate_interior_minimizers(w, k):
+    """Calculates all interior minimizers for the window size = w + k -
+    1 and inserts them into MINIMIZERS list."""
     window_size = w + k - 1
     for i in range(0, len(CURRENT_READ) - window_size + 1):
         minimizer = get_minimizer_from_window(i, i + window_size, READ_NUMBER, w, k)
@@ -147,17 +160,26 @@ def main():
     """Main method that calls everything else that gets the job done."""
     global READ_NUMBER
     global CURRENT_READ
+    global READ_LENGTH
     global USING_RC
+    global TOTAL_READS
 
     start = time.time()
 
-    w = 20
-    k = 20
-
-    if len(sys.argv) > 1:
-        filename = sys.argv[1]
+    if len(sys.argv) > 3:
+        w = int(sys.argv[1])
+        k = int(sys.argv[2])
+        filename = sys.argv[3]
     else:
         filename = 'ecoli100k.fa'
+        w = 20
+        k = 20
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if not line[0] == '>':
+                TOTAL_READS += 1
+
     with open(filename, 'r') as file:
         for line in file:
             line = line.strip()
@@ -168,28 +190,36 @@ def main():
                 else:
                     USING_RC = False
                     CURRENT_READ = line
-                    get_interior_minimizers(w, k)
-                    get_end_minimizers(k)
+                    READ_LENGTH = len(CURRENT_READ)
+                    calculate_interior_minimizers(w, k)
+                    calculate_end_minimizers(k)
 
                     USING_RC = True
                     CURRENT_READ = get_rc(line)
-                    get_interior_minimizers(w, k)
-                    get_end_minimizers(k)
+                    calculate_interior_minimizers(w, k)
+                    calculate_end_minimizers(k)
 
-                    if READ_NUMBER % 10 == 0:
+                    if READ_NUMBER % 500 == 0:
                         print('Read ' + str(READ_NUMBER) + '...')
 
                     READ_NUMBER += 1
 
-    print('%d distinct minimizers in total' % len(MINIMIZERS))
-    print_minimizers(20)
+    # saving to file only if filename passed from stdin
+    if len(sys.argv) > 1:
+        with open(filename + '_res.txt', 'w') as file:
+            for minimizer in MINIMIZERS:
+                file.write(str(minimizer) + '\n')
+        print('All minimizers saved to ' + filename + '_res.txt.')
+    else:
+        print('%d distinct minimizers in total' % len(MINIMIZERS))
+        print_minimizers(20)
 
-    # assuming 1 byte per letter
-    memory = k * len(MINIMIZERS)
-    for minimizer in MINIMIZERS:
-        # assuming 1 byte per index per location
-        memory += 2 * len(minimizer.get_locations())
-    print('Memory used: %s B' % memory)
+##    # assuming 1 byte per letter
+##    memory = k * len(MINIMIZERS)
+##    for minimizer in MINIMIZERS:
+##        # assuming 1 byte per index per location
+##        memory += 3 * len(minimizer.get_locations())
+##    print('Memory used: %s B' % memory)
 
     end = time.time()
     total_time = end - start
@@ -199,6 +229,8 @@ def main():
 CURRENT_READ = ''
 MINIMIZERS = []
 READ_NUMBER = 0
+READ_LENGTH = 0
+TOTAL_READS = 0
 USING_RC = False
 
 if __name__ == '__main__':
